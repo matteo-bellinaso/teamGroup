09bf42a2e6754fa592d4bdb8fa84847e9dcedd43
DavideBelvedere <davide.belvedere@gmail.com>
2018-03-21 09:58:15 +0100

    component creati e cartelle impostate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DetailComponent } from './detail/detail.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditComponent } from './edit/edit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DetailComponent,
+    HomeComponent,
+    ListComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
